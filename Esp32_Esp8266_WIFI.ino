289ecfcea6c1a713dd7b35aff7ad40f7ed9b6c99
skyrimXI <riteshpradhan55@gmail.com>
2023-11-18 14:09:40 +0530

    Menu Items for Scan

--- Esp32_Esp8266_WIFI.ino
+++ Esp32_Esp8266_WIFI.ino
@@ -1,7 +1,7 @@
 #include <SimpleButton.h>
 #include <TFT_eSPI.h>
 
-//=======================================Button & TFT ============================================//
+//======================================Button & TFT ==============================================//
 using namespace simplebutton;
 Button* UP = NULL;
 Button* DOWN = NULL;
@@ -78,9 +78,9 @@ void page_RootMenu(void){
   tft.setCursor(0, 30);
   if (root_pos == 1) {  tft.print("|>> ");  tft.println("SCAN");} else {  tft.println("     SCAN");}
   tft.setCursor(0, 40);
-  if (root_pos == 2) {  tft.print("|>> "); tft.println("ATTACK");} else {  tft.println("     ATTACK");}
+  if (root_pos == 2) {  tft.print("|>> "); tft.println("SNIFFER");} else {  tft.println("     SNIFFER");}
   tft.setCursor(0, 50);
-  if (root_pos == 3) {  tft.print("|>> ");  tft.println("SNIFFER");} else { tft.println("     SNIFFER");}
+  if (root_pos == 3) {  tft.print("|>> ");  tft.println("ATTACK");} else { tft.println("     ATTACK");}
   tft.setCursor(0, 60);
   if (root_pos == 4) {  tft.print("|>> ");  tft.println("WAR DRIVE");} else {  tft.println("     WAR DRIVER");}
   tft.setCursor(0, 70);
@@ -148,8 +148,83 @@ void page_RootMenu(void){
 }
 
 //====================================================================================//
+//||                                    Submenu1 = Scan option                       ||// 
+//====================================================================================//
 void page_SubMenu1(void){
-
+  boolean updateDisplay = true;
+  boolean staticElementsDrawn = false;
+  uint32_t loopStartMs;
+  int sub_pos = 1;
+  boolean isUpButtonPressed = false;
+  boolean wasUpButtonPressed = false;
+  boolean isDownButtonPressed = false;
+  boolean wasDownButtonPressed = false;
+  boolean isAcceptButtonPressed = false;
+  boolean wasAcceptButtonPressed = false;
+    while (currPage == SUB_MENU1) {
+    loopStartMs = millis();
+    if (!staticElementsDrawn) {
+    tft.fillScreen(TFT_BLACK);
+    tft.fillRoundRect(5, 5, 118, 20, 2, 0x0410);
+    tft.setCursor(10, 10);
+    tft.setTextColor(TFT_BLACK, 0x0410);
+    tft.print("SCAN");
+    staticElementsDrawn = true; 
+  }
+  if (updateDisplay) { 
+  tft.fillRect(0, 30, 128, 138, TFT_BLACK);
+  tft.setTextColor(TFT_WHITE, TFT_BLACK);
+  tft.setCursor(0, 30);
+  if (sub_pos == 1) {  tft.print("|>> ");  tft.println("BACK");} else {  tft.println("     BACK");}
+  tft.setCursor(0, 40);
+  if (sub_pos == 2) {  tft.print("|>> "); tft.println("SCAN FOR APs");} else {  tft.println("     SCAN FOR APs");}
+  tft.setCursor(0, 50);
+  if (sub_pos == 3) {  tft.print("|>> ");  tft.println("SCAN STATIONs");} else { tft.println("     SCAN STATIONs");}
+    }
+    tft.startWrite();
+//=============================Update buttons=======================//
+    UP->update();
+    isUpButtonPressed = UP->clicked();
+    DOWN->update();
+    isDownButtonPressed = DOWN->clicked();
+    RIGHT->update();
+    LEFT->update();
+    ACCEPT->update();
+    isAcceptButtonPressed = ACCEPT->clicked();
+//========================UP button handling========================//
+    if (isUpButtonPressed && !wasUpButtonPressed) {
+      sub_pos--;
+      if (sub_pos < 1) {
+        sub_pos =3;
+      }
+      updateDisplay = true;
+        }
+//======================DOWN button handling========================//
+    if (isDownButtonPressed && !wasDownButtonPressed) {
+      sub_pos++;
+      if (sub_pos > 3) {
+        sub_pos = 1;
+      }
+      updateDisplay = true;  
+        }  
+//==========================ACCEPT BUTTON HANDLING==============//
+        if (isAcceptButtonPressed && !wasAcceptButtonPressed) {
+        switch (sub_pos) {
+        case 1: currPage = ROOT_MENU;   break;    //RETURN TO BACK MENU
+        case 2: currPage = SUB_MENU2;   break;    //CODE TO SCAN FOR APs
+        case 3: currPage = SUB_MENU3;   break;    //CODE TO SCAN FOR STATION
+      }
+      updateDisplay = true;
+    }
+      while (millis() - loopStartMs < 25) {
+      delay(20);
+    }
+    tft.endWrite();
+    wasUpButtonPressed = isUpButtonPressed;
+    wasDownButtonPressed = isDownButtonPressed;
+    wasAcceptButtonPressed = isAcceptButtonPressed;
+    delay(10);  
+  }
 }
 //====================================================================================//
 void page_SubMenu2(void){
